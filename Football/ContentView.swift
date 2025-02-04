61d45b565dcb0f284eee0c8687a8da0cb95c0d6a
PedroHernandez173 <Phernande0152@gmail.com>
2025-02-04 13:36:23 -0800

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Student2 on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo)
+                Image(systemName: "figure.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
